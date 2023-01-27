2eabe65c0c4e5bdb8d163bfcde67e779dd923d80
shrimaliashish <ashishshrimali9935@gmail.com>
2023-01-28 00:06:20 +0530

    Time: 204 ms (85.48%), Space: 48.4 MB (61.45%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -7,10 +7,11 @@ public:
         }
         vector<string>res;
         for(auto word:words){
-            int n=word.length();
+            int n=word.length(); 
+            //that unknown part started which I was talking about
             vector<int>dp(n+1,0);
+            // ith position of this vector defines that till ith position it can be made from some word from the list
             dp[0]=1;
-
             for(int i=0;i<n;i++){
                 if(!dp[i]) continue;
                 
@@ -19,11 +20,13 @@ public:
                         dp[j]=1;
                     }
                 }
+                // adding word to the list if that satisfies our condition
                 if(dp[n]){
                     res.push_back(word);
                     break;
                 }
             }
+            //that unknown part started which I was talking about
         }
         return res;
     }
