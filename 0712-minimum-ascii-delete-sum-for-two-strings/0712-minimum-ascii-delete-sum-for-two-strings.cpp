8d6f8a60ea266520793d63dfe71874d92e80b001
shrimaliashish <ashishshrimali9935@gmail.com>
2023-03-19 16:09:00 +0530

    Time: 28 ms (85.80%), Space: 14.8 MB (76.26%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -26,6 +26,27 @@ public:
 //             return mx-k;
 // }
     
+//     int minimumDeleteSum(string s1, string s2) {
+//         int mx=0;
+//         for(auto x:s1)
+//             mx+=int(x);
+//         for(auto x:s2)
+//             mx+=int(x);
+//         int n1=s1.size(),n2=s2.size();
+//         vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+//         for(int i=1;i<n1+1;i++){
+//             for(int j=1;j<n2+1;j++){
+//                 int take=0,notTake=0;
+//                 if(s1[i-1]==s2[j-1]){
+//                       take=2*s1[i-1]+dp[i-1][j-1];
+//                 }
+//                 notTake=max(dp[i-1][j],dp[i][j-1]);
+//                 dp[i][j]=max(take,notTake);
+//             }
+//         }
+//         return mx-dp[n1][n2];
+//     }
+    
     int minimumDeleteSum(string s1, string s2) {
         int mx=0;
         for(auto x:s1)
@@ -33,18 +54,21 @@ public:
         for(auto x:s2)
             mx+=int(x);
         int n1=s1.size(),n2=s2.size();
-        vector<vector<int>>dp(n1+1,vector<int>(n2+1,0));
+        vector<int>dp(n2+1,0);
         for(int i=1;i<n1+1;i++){
+            vector<int>tmp(n2+1,0);
             for(int j=1;j<n2+1;j++){
                 int take=0,notTake=0;
                 if(s1[i-1]==s2[j-1]){
-                      take=2*s1[i-1]+dp[i-1][j-1];
+                      take=2*s1[i-1]+dp[j-1];
                 }
-                notTake=max(dp[i-1][j],dp[i][j-1]);
-                dp[i][j]=max(take,notTake);
+                notTake=max(dp[j],tmp[j-1]);
+                tmp[j]=max(take,notTake);
             }
+            dp=tmp;
         }
-        return mx-dp[n1][n2];
+        return mx-dp[n2];
         
     }
+    
 };
