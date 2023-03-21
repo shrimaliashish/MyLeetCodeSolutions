50e5eb58ba74f73c85b6026b681b4296871fb978
shrimaliashish <ashishshrimali9935@gmail.com>
2023-03-21 12:30:21 +0530

    Time: 93 ms (11.62%), Space: 302.7 MB (9.90%) - LeetHub

---
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+//     int fun(int i,int j,int n,vector<vector<int>>dp){
+//         if(i==n)
+//             return 0;
+//         if(i>n)
+//             return 10000;
+//         if(dp[i][j]!=-1){
+//             return dp[i][j];
+//         }
+//         int copy=10000,paste=10000;
+//         if(j<i)
+//         copy=1+fun(i,i,n,dp);
+//         if(j!=0)
+//         paste=1+fun(i+j,j,n,dp);
+//         return dp[i][j]= min(copy,paste);
+//     }
+    int getMinimum(int i,int j,int n,vector<vector<int>>&dp){
+        if(i==n)
+        return 0;
+      if(i>n)
+        return 10000;
+        if(dp[i][j]!=-1)
+           return dp[i][j];
+      if(i==j)
+        return 1+getMinimum(i+j,j,n,dp);
+      if(n==1)
+        return 0;
+      if(j==0)
+      return 1+getMinimum(i,i,n,dp);
+      else
+      return dp[i][j]= min(1+getMinimum(i,i,n,dp),1+getMinimum(i+j,j,n,dp));
+    }
+    int minSteps(int n) {
+        vector<vector<int>>dp(n+1,vector<int>(2*n+1,-1));
+        return getMinimum(1,0,n,dp);
+    }
+};
