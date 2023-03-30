a43d03d1ddfc8b2750ab5c1ab4d790fbb7a778ef
shrimaliashish <ashishshrimali9935@gmail.com>
2023-03-30 22:22:55 +0530

    Time: 13 ms (76.67%), Space: 12.1 MB (46.71%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    int ans=0;
-    // vector<vector<int>>dp;
-    map<pair<int,int>,int>dp;
-    int fun(int i,int target,vector<int>& nums){
-        if(i<0 && target==0)
-            return 1;
-        if(i<0)
-            return 0;
-        if(dp.count({i,target}))
-            return dp[{i,target}];
-        int cnt=0;
-        cnt+=fun(i-1,target-nums[i],nums);
-        cnt+=fun(i-1,target+nums[i],nums);
-        return dp[{i,target}]=cnt;
+    int f(int i, int tar, vector<int>&nums, vector<vector<int>>&dp) {
+        if(i==0) {
+            if(tar==0 && nums[0] == 0) return 2;
+            if(tar==nums[0] || tar==0) return 1;
+            else return 0;
+        }
+        if(dp[i][tar]!=-1) return dp[i][tar];
+        int notTake = f(i-1, tar, nums, dp);
+        int take = 0;
+        if(tar >= nums[i]) take = f(i-1, tar-nums[i], nums, dp);
+        return dp[i][tar] = (take+notTake);
+    }
+    int findWays(vector<int> &nums, int tar){
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(tar+1, -1));
+        return f(n-1, tar, nums, dp);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        // dp.resize(n,vector<int>(target+1,-1));
-        return fun(n-1,target,nums);
+        int totSum = accumulate(nums.begin(), nums.end(), 0);
+        if(totSum - target < 0 || (totSum-target)%2 != 0) return 0;
+        return findWays(nums, (totSum - target)/2);
     }
 };
