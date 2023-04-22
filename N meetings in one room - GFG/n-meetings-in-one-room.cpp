4d1be1ec024e75d13bce32a1e41299f637ea335e
shrimaliashish <ashishshrimali9935@gmail.com>
2023-04-23 02:05:33 +0530

    Added solution - LeetHub

---
+++ N meetings in one room - GFG/n-meetings-in-one-room.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of meetings that can
+    //be performed in a meeting room.
+    static bool cmp(pair<int,int>&p1,pair<int,int>&p2){
+        return p1.second<p2.second;
+    }
+    int maxMeetings(int start[], int end[], int n)
+    {
+        vector<pair<int,int>>v;
+        for(int i=0;i<n;i++){
+            v.push_back({start[i],end[i]});
+        }
+        sort(v.begin(),v.end(),cmp);
+        int prev=-1,cnt=0;
+        for(auto x:v){
+            if(x.first>prev){
+                cnt++;
+                prev=x.second;
+            }
+        }
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int start[n], end[n];
+        for (int i = 0; i < n; i++) cin >> start[i];
+
+        for (int i = 0; i < n; i++) cin >> end[i];
+
+        Solution ob;
+        int ans = ob.maxMeetings(start, end, n);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
